40e761069bc1b01dce920ada2b3d7f5c4e5ffd9a
Kenji Mah <kemmah@ucsc.edu>
2019-01-18 23:35:43 -0800

    initial loginish

--- SkateApp/SkateApp/StartViewController.swift
+++ SkateApp/SkateApp/StartViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class StartViewController: UIViewController {
+    
+    @IBOutlet var signUp: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
