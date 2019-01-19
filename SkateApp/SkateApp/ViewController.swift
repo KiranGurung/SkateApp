f15ee8532f6b3e3638b1993a79ee8df949c5cdaf
Kenji Mah <kemmah@ucsc.edu>
2019-01-19 13:15:10 -0800

    camera test

--- SkateApp/SkateApp/ViewController.swift
+++ SkateApp/SkateApp/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var previewView: UIView!
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized: // The user has previously granted access to the camera.
+            setupCaptureSession()
+            
+        case .notDetermined: // The user has not yet been asked for camera access.
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    self.setupCaptureSession()
+                }
+            }
+            
+        case .denied: // The user has previously denied access.
+            return
+        case .restricted: // The user can't grant access due to restrictions.
+            return
+        }
+    }
+    
+    func setupCaptureSession(){
+        
+        captureSession = AVCaptureSession()
+        if let captureDevice = AVCaptureDevice.default(for: .video){
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice)
+                captureSession?.addInput(input)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+                videoPreviewLayer?.frame = view.layer.bounds
+                previewView.layer.addSublayer(videoPreviewLayer!)
+                captureSession?.startRunning()
+            } catch{
+                print(error)
+            }
+        }else {
+            print("oh no")
+        }
     }
 
     override func didReceiveMemoryWarning() {
